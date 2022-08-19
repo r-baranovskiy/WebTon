778e1cf57721a90b410e4eb0367f48147c0ecf70
Ruslan <rus.baranovskiy@gmail.com>
2022-08-20 00:04:25 +0300

    Initial Commit

---
+++ WebTon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebTon
+//
+//  Created by Руслан Барановский on 20.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
