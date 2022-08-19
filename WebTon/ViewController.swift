efafe242ff8e7e8a5ff07800e4c97d6bf7c82b4d
Ruslan <rus.baranovskiy@gmail.com>
2022-08-20 02:01:27 +0300

    All UI elements was added Keyboard can hide when it's !selected Webview goes up when keyboard selected

--- WebTon/ViewController.swift
+++ WebTon/ViewController.swift
@@ -1,19 +1,74 @@
-//
-//  ViewController.swift
-//  WebTon
-//
-//  Created by Руслан Барановский on 20.08.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var bottomScrollConstraint: NSLayoutConstraint!
+    @IBOutlet weak var webScrollView: UIScrollView!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    //MARK: - Lifecycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        registerForKeyboardNotifications()
+        
     }
+    
+    //MARK: - IBActions button
+    
+    @IBAction func yandexButtonPressed() {
+        hideKeyboard()
+        searchTextField.text = nil
+    }
+    
+    @IBAction func youtubeButtonPressed() {
+        hideKeyboard()
+    }
+    
+    @IBAction func backButtonPressed() {
+        hideKeyboard()
+    }
+    
+    
+    //MARK: - Settings keyboard
+    
+    private func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(_ notification: NSNotification) {
+        guard let userInfo = notification.userInfo,
+              let animationDuration = (userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue,
+              let keyboardScreenEndFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            bottomScrollConstraint.constant = 0
+        } else {
+            bottomScrollConstraint.constant = keyboardScreenEndFrame.height + 10
+        }
+        
+        view.needsUpdateConstraints()
+        UIView.animate(withDuration: animationDuration) {
+            self.view.layoutIfNeeded()
+        }
+    }
+}
 
+//MARK: - Extensions
 
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        hideKeyboard()
+        return true
+    }
+    
+    func hideKeyboard() {
+        view.endEditing(true)
+    }
 }
 
