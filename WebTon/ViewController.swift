52ca6a4f865f0897ae67d70ed277a63a08527981
Ruslan <rus.baranovskiy@gmail.com>
2022-08-20 03:00:58 +0300

    Now browser can open URL links which added to app

--- WebTon/ViewController.swift
+++ WebTon/ViewController.swift
@@ -1,37 +1,66 @@
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    
+    //MARK: - Constants
+    
+    private var browserWebView = WKWebView()
+    private var previousURL = String()
     
     //MARK: - Outlets
     
     @IBOutlet weak var bottomScrollConstraint: NSLayoutConstraint!
     @IBOutlet weak var webScrollView: UIScrollView!
     @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var webView: UIView!
+    @IBOutlet weak var mainView: UIView!
     
     //MARK: - Lifecycles
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         registerForKeyboardNotifications()
-        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        openWebBrowser()
     }
     
     //MARK: - IBActions button
     
     @IBAction func yandexButtonPressed() {
+        openURLlink(address: "http://yandex.ru")
         hideKeyboard()
         searchTextField.text = nil
     }
     
     @IBAction func youtubeButtonPressed() {
+        openURLlink(address: "http://youtube.com")
         hideKeyboard()
+        searchTextField.text = nil
     }
     
     @IBAction func backButtonPressed() {
         hideKeyboard()
     }
     
+    //MARK: - Functions
+    
+    private func openWebBrowser() {
+        browserWebView = WKWebView(frame: webView.frame)
+        webView.addSubview(browserWebView)
+    }
+    
+    private func openURLlink(address: String) {
+        let address = address
+        guard let url = URL(string: address) else { return }
+        let request = URLRequest(url: url)
+        
+        browserWebView.load(request)
+    }
     
     //MARK: - Settings keyboard
     
@@ -50,7 +79,9 @@ class ViewController: UIViewController {
         } else {
             bottomScrollConstraint.constant = keyboardScreenEndFrame.height + 10
         }
-        
+//        mainView.needsUpdateConstraints()
+//        webView.needsUpdateConstraints()
+//        browserWebView.needsUpdateConstraints()
         view.needsUpdateConstraints()
         UIView.animate(withDuration: animationDuration) {
             self.view.layoutIfNeeded()
