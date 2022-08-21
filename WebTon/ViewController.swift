fc4c1068842f3c355dedf63082d6825cd8abfa3f
Ruslan <rus.baranovskiy@gmail.com>
2022-08-21 12:08:12 +0300

    edited UI elements

--- WebTon/ViewController.swift
+++ WebTon/ViewController.swift
@@ -35,6 +35,7 @@ class ViewController: UIViewController {
         super.viewDidAppear(animated)
         openWebBrowser()
         openURLlink(address: "http://onliner.by")
+        backButton.isSelected = false
     }
     
     
